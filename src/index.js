0c056758e738ebf8f27ffad387add17f164e79ba
Skanner33 <mathewmageto@gmail.com>
2022-12-06 12:59:22 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    workToDo(e.target)
+  })
 });
+
+function workToDo(todo) {
+ 
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  
+  li.innerHTML = todo
+  
+  document.querySelector('ul').appendChild(li);
+
+  document.querySelector('li').appendChild(btn);
+
+  document.querySelector('ul').appendChild(li);
+
+  document.querySelector('li').remove
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
